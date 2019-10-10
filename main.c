6a0b7232e2268d31fcaa6d7aac91233c82adb07e
z2yeon <55427811+z2yeon@users.noreply.github.com>
2019-10-10 16:11:11 +0900

    06_01

--- main.c
+++ main.c
@@ -4,37 +4,19 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 //함수정의 
 
-int sumTwo(int a,int b)
+void print_star()
 {
-	int output;
-	output=a+b;
-	return output;
-}
-
-int square(int n)
-{
-	return(n*n);
-}
-
-int get_max(int a, int b)
-{
-	if(a>b)return(a);
-	else return(b);
+	int i;
+	for (i=0;i<10;i++)
+	 printf("*");
 }
 	
 //함수호출	
-int main(int argc, char *argv[]) {
-	
-	int x,y;
-	int output;
-	
-	x=2,y=5;
-	
-	output=sumTwo(x,y);
-	printf("sumTwo:%i\n",output);
-	printf("square:%d\n",square(x));
-	printf("get_max:%d\n",get_max(x,y));
-	
+int main(void){
 
+    print_star();
+    print_star();
+    print_star();
+    
 	return 0;
 }
